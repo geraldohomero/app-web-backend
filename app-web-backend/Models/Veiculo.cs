445150431bbcaf1dbe7186b29705acdd27b33b62
Geraldo Homero <geraldohomero@protonmail.com>
2023-04-21 16:59:49 -0300

    update

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    [Table("Veículo")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o Nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a Placa!")]
+        public string Placa { get; set; }
+
+
+       // public ICollection<Consumo> Consumos { get; set; }
+
+        ; }
+    }
+}
